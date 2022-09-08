93d6a2ed8302d1218792095484f9b46e3ec3fd09
Saumya-196039H <saumyanikethani@gmail.com>
2022-09-08 21:45:59 +0530

    changed bubble sort

---
+++ bubble_sort_input.py
@@ -0,0 +1,16 @@
+def b_sort(list):
+
+for iter_num in range(len(list)-1,0,-1):
+    for idx in range(iter_num):
+
+        if list[idx] > list[idx+1]:
+            temp = list[idx]
+            list[idx] = list[idx+1]
+            list[idx+1] = temp
+
+str_input= input("Enter numbers:  ")
+list = [int(x) for x in str_input.split()]
+
+b_sort(list)
+print('sorted elements are: ')
+print(list)
